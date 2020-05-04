75e542f60e20a009f5501048fc5cb922286379ba
Alexander Ignatov <ani1802@abv.bg>
2020-05-04 18:37:09 +0300

    Initial commit

---
+++ config/config.php
@@ -0,0 +1,19 @@
+<?php
+
+define("SETTINGS", [
+
+    // Database settings
+    "DATABASE_HOST" => "localhost",
+    "DATABASE_USER" => "root",
+    "DATABASE_PASSWORD" => "",
+    "DATABASE_NAME" => "WWW_PROJECT_62136_62152_62177",
+
+    // HTML Templates
+    "TEMPLATES_DIR" => "templates",
+    "TEMPLATE_NAME" => "main",
+
+    // File upload
+    "MAX_FILESIZE_BYTES" => 5_000_000,
+    "ALLOWED_PHOTO_FORMATS" => ["png", "jpg", "jpeg", "gif"],
+
+]);
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once "config/config.php";
+
+// TODO
+
+$template_dir = SETTINGS["TEMPLATES_DIR"] . "/" . SETTINGS["TEMPLATE_NAME"];
+include "$template_dir/index.html";
