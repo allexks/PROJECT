edb266862b36d79e343492e2114909592ba08b80
Alexander Ignatov <ani1802@abv.bg>
2020-05-09 18:18:28 +0300

    Check test results

--- view.php
+++ view.php
@@ -62,17 +62,126 @@ if ($test->user_id != $user_id) {
     $view->send();
 }
 
+
+$question_classes = [];
+$sum = 0;
+$max = 0;
+$is_viewing_results = false;
+
 // Perform results check if the form is submitted
 
 if (isset($_POST["submit"])) {
 
-    // TODO
+    $is_viewing_results = true;
+
+    // Determine right and wrongly answered questions
+
+    foreach ($_POST as $key => $value) {
+
+        // Answers to the questions are in arrays named "qXXX" where "XXX" is the id of the question
+
+        if (!preg_match("/^[qQ][0-9]+$/", $key)) {
+            continue;
+        }
+
+        $question_id = substr($key, 1, strlen($key) - 1) ?? "0";
+        $question_id = (int)$question_id;
+
+        if (!$question_id) {
+            continue;
+        }
+
+        // Find the question from the ones we have fetched with the test
+
+        $question = null;
+        foreach ($test->questions as $ind_q => $q) {
+            if ($q->id == $question_id) {
+                $question = $q;
+                break;
+            }
+        }
+
+        if (!$question) {
+            continue;
+        }
+
+        $correct_count = 0;
+
+        foreach ($value as $ind_ag => $answer_given) {
+
+            // Given answers are in the "aXXX" format where "XXX" is the id of the answer
+
+            if (!preg_match("/^[aA][0-9]+$/", $answer_given)) {
+                continue;
+            }
+
+            $answer_id = substr($answer_given, 1, strlen($answer_given) - 1) ?? "0";
+            $answer_id = (int)$answer_id;
+
+            if (!$answer_id) {
+                continue;
+            }
+
+            // Find the answer among the real ones
+
+            foreach ($question->answers as $ind_a => $answer) {
+                if ($answer->id != $answer_id) {
+                    continue;
+                }
+
+                if ($answer->is_correct) {
+                    $correct_count++;
+                } else {
+                    // Even one wrong answer is enough to mark the question as wrongly answered.
+                    $correct_count = 0;
+                    break;
+                }
+            }
+        }
+
+        $all_possible_count = 0;
+
+        // Calculate all possible right answers for this question
+
+        foreach ($question->answers as $ind_a => $answer) {
+            if ($answer->is_correct) {
+                $all_possible_count++;
+            }
+        }
+
+        // Determine the question status
+
+        $ratio = $correct_count / $all_possible_count;
+
+        $question_class = "partial";
+
+        if ($ratio == 1) {
+            $question_class = "correct";
+        } elseif ($ratio == 0) {
+            $question_class = "wrong";
+        }
+
+        $question_classes[(string)$question_id] = $question_class;
+        $sum += $correct_count;
+        $max += $all_possible_count;
+    }
 }
 
 $params = [
     "title" => $test->title ?? "",
     "questions" => $test->questions ?? [],
+    "question_classes" => $question_classes,
+    "sum" => $sum,
+    "max" => $max,
+    "is_viewing_results" => $is_viewing_results,
 ];
+
+foreach ($params["questions"] as $ind_q => $question) {
+    $qkey = (string)$question->id;
+    if (!isset($params["question_classes"][$qkey])) {
+        $params["question_classes"][$qkey] = "";
+    }
+}
 
 $view = new View("view_test");
 $view->send($params);
