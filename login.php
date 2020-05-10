45288fccfab4593a5689147810d6948e98852c68
root <antoni.1998@abv.bg>
2020-05-10 16:33:29 +0300

    Create login form

--- login.php
+++ login.php
@@ -1,8 +1,20 @@
 <?php
 
 require_once "classes/util/View.class.php";
+require_once "classes/util/Database.class.php";
+
+session_start();
+
 $view = new View("login");
 
-// TODO
+// Establish database connection
+
+$database = new Database();
+$db = $database->getNewConnection();
+
+if (!$db) {
+    $view = new View("database_error");
+    $view->send();
+}
 
 $view->send();
