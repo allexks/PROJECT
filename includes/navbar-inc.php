e9a464016af1ad11bc07f5eedfac882321384eaf
Betina Borisova <b.borisowa17@gmail.com>
2020-05-15 10:30:24 +0300

    Fixed issues, added check if the user is logged in when displaying the navigation bar and added additional checks in 'import.php'.

--- includes/navbar-inc.php
+++ includes/navbar-inc.php
@@ -1,19 +1,20 @@
 <!DOCTYPE html>
 
 <html>
-	<!-- За обикновен потребител
-	<div class="navbar">
-		<img class="position-left" src="images/logo.png">
-		<a class="positio-left" href="browse.php">Начало</a>
-		<a class="position-right" href="register.php">Регистрация</a>
-		<a class="position-right" href="login.php">Вход</a>
-	</div> -->
-	<!-- За логнат потребител -->
-	<div class="navbar">
-		<img class="position-left" src="images/logo.png">
-		<a class="position-left" href="browse.php">Начало</a>
-		<a class="position-left" href="import.php">Импорт</a>
-		<a class="position-left" href="my-tests.php">Моите тестове</a>
-		<a class="position-right" href="exit.php">Изход</a>
-	</div>
+	<?php if (isset($_SESSION["user_id"]) && $_SESSION["user_id"] > 0) : ?>
+		<div class="navbar">
+			<img class="position-left" src="images/logo.png">
+			<a class="position-left" href="index.php">Начало</a>
+			<a class="position-left" href="import.php">Импорт</a>
+			<a class="position-left" href="browse.php">Моите тестове</a>
+			<a class="position-right" href="exit.php">Изход</a>
+		</div>
+	<?php else : ?>
+		<div class="navbar">
+			<img class="position-left" src="images/logo.png">
+			<a class="positio-left" href="index.php">Начало</a>
+			<a class="position-right" href="signup.php">Регистрация</a>
+			<a class="position-right" href="login.php">Вход</a>
+		</div>
+	<?php endif;?>
 </html>
